74494cf5ae2faedae492ea844b9b159f46c82501
Ryan Sears <ryan@calidog.io>
2021-09-14 13:36:56 -0400

    Allow for network selection and change page title

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
 <template>
   <div id="app">
-    <iframe class="datadog" src="https://p.datadoghq.com/sb/e72980047-2d709421ba52ff347142795b6c9b92e2?theme=dark&tpl_var_Network=mainnet" />
+    <iframe
+      :key="network"
+      class="datadog"
+      :src="`https://p.datadoghq.com/sb/e72980047-2d709421ba52ff347142795b6c9b92e2?theme=dark&tpl_var_Network=${network}`"
+    />
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+
+enum networks {
+  MAIN = 'mainnet',
+  GRANADA = 'granadanet'
+}
+
+export default class MainPage extends Vue {
+  private network = networks.MAIN
+
+  mounted(): void {
+    const newNetwork = this.$route.query.network
+    if (Object.values(networks).includes(newNetwork as networks) && newNetwork !== 'mainnet'){
+      this.network = newNetwork as networks
+    } else {
+      // Redirect without any query to default to mainnet
+      this.$router.push(this.$route.path)
+    }
+  }
+}
+</script>
+
 <style lang="scss">
-.datadog {
- height: 100vh;
- width: 100vw;
- border: none;
-}
+  .datadog {
+   height: 100vh;
+   width: 100vw;
+   border: none;
+  }
 
-body {
-  margin: 0;
-}
+  body {
+    margin: 0;
+  }
 
-#app {
-  height: 100vh;
-  width: 100vw;
-}
+  #app {
+    height: 100vh;
+    width: 100vw;
+  }
 </style>
