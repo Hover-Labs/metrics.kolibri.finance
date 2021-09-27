d84bee88699cbd87e35eef6b3c16b9adda98f73e
Ryan Sears <ryan@calidog.io>
2021-09-27 16:12:16 -0400

    Disable x-frame-options for metrics.kolibri.finance

---
+++ workers-site/index.js
@@ -0,0 +1,72 @@
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
+
+/**
+ * The DEBUG flag will do two things that help during development:
+ * 1. we will skip caching on the edge, which makes it easier to
+ *    debug.
+ * 2. we will return an error message on exception in your Response rather
+ *    than the default 404.html page.
+ */
+const DEBUG = false
+
+addEventListener('fetch', event => {
+  try {
+    event.respondWith(handleEvent(event))
+  } catch (e) {
+    if (DEBUG) {
+      return event.respondWith(
+        new Response(e.message || e.toString(), {
+          status: 500,
+        }),
+      )
+    }
+    event.respondWith(new Response('Internal Error', { status: 500 }))
+  }
+})
+
+function addSecurityHeaders(response){
+  response.headers.set('X-XSS-Protection', '1; mode=block')
+  response.headers.set('X-Content-Type-Options', 'nosniff')
+  // response.headers.set('X-Frame-Options', 'SAMEORIGIN')
+  response.headers.set('Referrer-Policy', 'same-origin')
+  response.headers.set('Feature-Policy', 'none')
+  response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains')
+  return response
+}
+
+async function handleEvent(event) {
+  let options = {}
+
+  try {
+    if (DEBUG) {
+      // customize caching
+      options.cacheControl = {
+        bypassCache: true,
+      }
+    }
+
+    const page = await getAssetFromKV(event, options)
+
+    // allow headers to be altered
+    const response = new Response(page.body, page)
+
+    return addSecurityHeaders(response)
+
+  } catch (e) {
+    // if an error is thrown try to serve the asset at 404.html
+    if (!DEBUG) {
+      try {
+        let notFoundResponse = await getAssetFromKV(event, {
+          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/index.html`, req),
+        })
+
+        let response = new Response(notFoundResponse.body, { ...notFoundResponse, status: 200 })
+        return addSecurityHeaders(response)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    return new Response(e.message || e.toString(), { status: 500 })
+  }
+}
